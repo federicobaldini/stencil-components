1f1c0f9928da1003f3670fd049ed7d2a8bcc1ddf
Federico Baldini <federico.baldini7@gmail.com>
2023-08-12 13:10:47 +0200

    Update side-drawer.tsx

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,14 +1,23 @@
-import { h, Component, JSX } from '@stencil/core';
+import { h, Component, JSX, Prop } from '@stencil/core';
 
 @Component({
   tag: 'wc-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true,
 })
 export class SideDrawer {
+  @Prop({ reflect: true }) title: string;
+
   public render(): JSX.Element {
     return (
-      <div>
-        <h1>The Side Drawer</h1>
-      </div>
+      <aside>
+        <header>
+          <h1>{this.title}</h1>
+        </header>
+        <main>
+          <slot></slot>
+        </main>
+      </aside>
     );
   }
 }
