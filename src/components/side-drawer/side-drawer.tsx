adb4b670c22f354bf61850ecb68afa58cd16f86c
Federico Baldini <federico.baldini7@gmail.com>
2023-08-16 00:23:49 +0200

    Update side-drawer.tsx

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -7,13 +7,18 @@ import { h, Component, JSX, Prop } from '@stencil/core';
 })
 export class SideDrawer {
   @Prop({ reflect: true }) title: string;
-  @Prop({ reflect: true }) open: boolean;
+  @Prop({ reflect: true, mutable: true }) open: boolean;
+
+  private closeHandler(): void {
+    this.open = false;
+  }
 
   public render(): JSX.Element | null {
     return (
       <aside class={`${this.open ? 'open' : ''}`}>
         <header>
           <h1>{this.title}</h1>
+          <button onClick={this.closeHandler.bind(this)}>X</button>
         </header>
         <main>
           <slot></slot>
